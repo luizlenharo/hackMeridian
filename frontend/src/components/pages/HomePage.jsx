03512837d63d9c7ea72c0ea55853b4538f2ff043
Fernando Nagano Foschiera <fnfoschiera@localhost.localdomain>
2025-09-15 20:07:37 -0300

    adiciona base do front

---
+++ frontend/src/components/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
+function HomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <div className="home-card">
+        <h1 className="home-title">Welcome to Food for All!</h1>
+        <p className="home-subtitle">
+          Do you wish to register as a client or restaurant?
+        </p>
+
+        <div className="button-group">
+          <button
+            className="choice-button"
+            onClick={() => navigate("/cadastro")}
+          >
+            Register as Client
+          </button>
+
+          <button
+            className="choice-button"
+            onClick={() => navigate("/cadastro-restaurante")}
+          >
+            Register as Restaurant
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// 1. Importe os componentes do react-router-dom
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.jsx';
+import LoginPage from './components/pages/login/LoginUsuario.jsx';
+import RegisterPage from './components/register/RegisterPage.jsx';
+
+import './index.css';
+import HomePage from './components/pages/HomePage.jsx';
+import ResgisterRestaurant from './components/register/RegisterRestaurant.jsx';
+import LoginRestaurant from './components/pages/login/LoginRestaurant.jsx';
+
+// 2. Crie a configuração das suas rotas
+const router = createBrowserRouter([
+  {
+    path: "/", // A rota raiz agora será a página de login
+    element: <HomePage />,
+  },
+  {
+    path: "/cadastro", // A rota para a nova página
+    element: <RegisterPage />,
+  },
+  // Você pode adicionar mais rotas aqui no futuro
+  {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+  {
+    path: "cadastro-restaurante",
+    element: <ResgisterRestaurant/>,
+  },
+  {
+    path: "login-restaurante",
+    element: <LoginRestaurant/>,
+  },
+]);
+
+// 3. Mude o render para usar o RouterProvider
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
