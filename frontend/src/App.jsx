ae70517a34cbcc8a5b082cc9f94b4d3c852fbadd
luiz lenharo <llenharo@ciandt.com>
2025-09-16 04:53:55 -0300

    fix: remove margin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import React from 'react';
+import UserHomePage from "./components/pages/user/UserHomePage.jsx"
 import RestaurantPage from "./components/pages/restaurant/RestaurantPage.jsx";function App() {
 
   // Por enquanto, nosso App apenas mostra a página de login.
